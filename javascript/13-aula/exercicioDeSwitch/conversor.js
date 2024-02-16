c1f7799c57602b30f0eb1755969b814753d75961
Pedro0000000000 <tnthenriquepedro@gmail.com>
2024-02-16 00:59:26 -0300

    Criação de um conversor de metro para melimetro, centimetro, decimetros, decâmetro, hectômetro e quilometro usando JavaScript

---
+++ javascript/13-aula/exercicioDeSwitch/conversor.js
@@ -0,0 +1,32 @@
+const medida = parseFloat(prompt("Insira uma medida em metros:"));
+
+const opcoesDeMedidas = prompt(
+  "Para qual unidade de medida você deseja converter? " +
+    "\n1 - Milimetros (mm)" +
+    "\n2 - Centimetros (cm)" +
+    "\n3 - Decimetro (dm)" +
+    "\n4 - Decâmetro (dam)" +
+    "\n5 - Hectrômetros (hm)" +
+    "\n6 - Quilometros (km)"
+);
+
+switch (opcoesDeMedidas) {
+  case "1":
+    alert("Resultado: " + medida + "m = " + medida * 1000 + "mm");
+    break;
+  case "2":
+    alert("Resultado: " + medida + "m = " + medida * 100 + "cm");
+    break;
+  case "3":
+    alert("Resultado: " + medida + "m = " + medida * 10 + "dm");
+    break;
+  case "4":
+    alert("Resultado: " + medida + "m = " + medida / 10 + "dam");
+    break;
+  case "5":
+    alert("Resultado: " + medida + "m = " + medida / 100 + "hm");
+    break;
+  case "6":
+    alert("Resultado: " + medida + "m = " + medida / 1000 + "km");
+    break;
+}
