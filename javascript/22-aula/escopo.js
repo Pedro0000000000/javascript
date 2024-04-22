b9b2f66778bf59a3ae6d12695422e50c8f82505b
Pedro0000000000 <tnthenriquepedro@gmail.com>
2024-04-22 00:02:08 -0300

    Criação da pasta aula-22 que contem os conteudos sobre escopo

---
+++ javascript/22-aula/escopo.js
@@ -0,0 +1,22 @@
+let pokemon = "Charmander 1.0";
+let dragon = "Dragon 1";
+
+function evoluir() {
+  pokemon = "Charmander 1.1";
+  dragon = "Dragon 2";
+  console.log(dragon);
+  return pokemon;
+}
+let novoPokemon = evoluir();
+
+console.log(novoPokemon);
+
+let gato = "Pull";
+function cat() {
+  gato += " Garfield";
+  console.log(gato);
+}
+
+console.log(gato);
+cat();
+console.log(gato);
