f7c533ab1b8459b4245dd4fbb1080874d098e6b9
Pedro0000000000 <tnthenriquepedro@gmail.com>
2024-05-23 23:56:57 -0300

    Criação do arquivo 29-Dom-aula que contem meu primeiro contato com a tecnica de integrar o JS com HTML

---
+++ javascript/29-Dom-aula/index.js
@@ -0,0 +1,19 @@
+function onMuseenter() {
+  console.log("Entrada do mouse.");
+}
+
+function onChange() {
+  console.log("Mudança do valor.");
+}
+
+function onFocus() {
+  console.log("Foco no elemento.");
+}
+
+function onBlur() {
+  console.log("Desfocado elemento.");
+}
+
+function onClick() {
+  console.log("Clicou no botão.");
+}
