25454cd49ec451765c9a2a3b857bd11bce33dacb
Pedro0000000000 <tnthenriquepedro@gmail.com>
2024-06-19 18:40:38 -0300

    Manipulando atributos com JavaScript

---
+++ javascript/29-Dom-aula/Manipulando-atributos-com-JS/maniAtributos.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("input");
+
+document.getElementById("value").addEventListener("click", function () {
+  input.value = "Olá Mundo!";
+  // input.value = input.value === "" ? "Olá Mundo!" : "";
+
+  console.log(input.value);
+  console.log(input.getAttribute("value"));
+});
+
+document.getElementById("type").addEventListener("click", function () {
+  input.type = input.type !== "date" ? "date" : "text";
+  // input.setAttribute("type", "radio");
+});
+
+document.getElementById("placeholder").addEventListener("click", function () {
+  input.placeholder = "Digite algo aqui!";
+});
+
+document.getElementById("disabled").addEventListener("click", function () {
+  input.setAttribute("disabled", !input.disabled);
+});
+
+document.getElementById("data").addEventListener("click", function () {
+  const data = input.dataset.something;
+  console.log("O valor do atributo data-something é: " + data);
+
+  input.dataset.something = "Algum outro valor!";
+  console.log(
+    "O valor do atributo data-something agora é: " + input.dataset.something
+  );
+});
